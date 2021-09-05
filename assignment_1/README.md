5f90b0aa444f5931bea29c70dcfcf88f1b242b71
Shiven <shiven@iitk.ac.in>
2021-09-06 02:27:17 +0530

    started outline for assignment 1

---
+++ assignment_1/README.md
@@ -0,0 +1,1 @@
+gym envs to solve assignment 1
---
+++ assignment_1/assignment_1/envs/__init__.py
@@ -0,0 +1,3 @@
+from assignment_1.envs.gaussianBandit import GaussianBandit
+from assignment_1.envs.bernoulliBandit import BernoulliBandit
+from assignment_1.envs.RWE import RWE
